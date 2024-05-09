e14335bfee6f19032453ae7850fa4ee6717ac296
Lahiru Chanaka <kodikaragelahiru@gmail.com>
2024-05-09 13:48:15 +0530

    final

--- src/main/java/lk/ijse/controller/roomcontroller.java
+++ src/main/java/lk/ijse/controller/roomcontroller.java
@@ -1,4 +1,26 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class roomcontroller {
+    public TextField txtId2;
+    public TextField txtId1;
+    public DatePicker date;
+    public TextField txtId;
+    public AnchorPane Load;
+
+    public void btnOnActionSave(ActionEvent actionEvent) {
+
+    }
+
+    public void btnOnActionUpdate(ActionEvent actionEvent) {
+
+    }
+
+    public void btnOnActionDelete(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/lk/ijse/controller/suppliercontroller.java
+++ src/main/java/lk/ijse/controller/suppliercontroller.java
@@ -1,2 +1,27 @@
-package lk.ijse.controller;public class suppliercontroller {
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class suppliercontroller {
+    public AnchorPane Load;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtContact;
+    public DatePicker date;
+
+    public void btnOnActionDelete(ActionEvent actionEvent) {
+
+    }
+
+    public void btnOnActionUpdate(ActionEvent actionEvent) {
+
+    }
+
+    public void btnOnActionSave(ActionEvent actionEvent) {
+
+    }
 }
