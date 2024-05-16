5de2e9981478293d81362c3639b2e22499080227
Deleesha Nethsara Dias Galgodella <deleesha02@gmail.com>
2024-05-16 11:23:25 -0700

    repoDone

--- src/main/java/lk/ijse/dreamcabana/controller/CreateFromcontroller.java
+++ src/main/java/lk/ijse/dreamcabana/controller/CreateFromcontroller.java
@@ -1,21 +1,31 @@
 package lk.ijse.dreamcabana.controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lk.ijse.dreamcabana.db.DbConnection;
+import lk.ijse.dreamcabana.util.ValidateUtil;
+
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
-public class CreateFromcontroller {
+public class CreateFromcontroller implements Initializable {
 
     @FXML
     private AnchorPane load;
@@ -29,8 +39,10 @@ public class CreateFromcontroller {
     @FXML
     private TextField txtuser;
 
+    private LinkedHashMap<TextField, Pattern> map = new LinkedHashMap<>();
+
     @FXML
-    void btnSignup(ActionEvent event) {
+    void btnSignup() {
         String user_id = txtuser.getText();
         String password = txtpw.getText();
 
@@ -45,7 +57,7 @@ public class CreateFromcontroller {
             PreparedStatement pstm = connection.prepareStatement(sql);
             pstm.setObject(1, user_id);
             pstm.setObject(2, password);
-            if(pstm.executeUpdate() > 0) {
+            if (pstm.executeUpdate() > 0) {
                 new Alert(Alert.AlertType.CONFIRMATION, "user saved!").show();
                 navigateToDashbord();
             }
@@ -65,4 +77,18 @@ public class CreateFromcontroller {
     }
 
 
+    public void signupKeyRelease(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            ValidateUtil.validation(map);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Pattern patternId = Pattern.compile("^[A-z]{4,10}$");
+        Pattern patternName = Pattern.compile("^[A-z 0-9 .]{3,}$");  //[0-9 a-z]{10}
+
+        map.put(txtuser, patternId);
+        map.put(txtpw, patternName);
+    }
 }
